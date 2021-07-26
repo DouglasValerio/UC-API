436e4c352b6fa3bef0e3de0bd2b2bd5372d7ac36
Douglas Valério <douglas.valerio@matilha.digital>
2021-07-26 09:37:58 -0300

    Move error 401 to a helper method

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,4 +1,5 @@
 const MissingParamsError = require('./missing-params-error')
+const UnauthorizedError = require('./unauthorized-error')
 module.exports = class HttpResponse {
   static badRequest (param) {
     return { statusCode: 400, body: new MissingParamsError(param) }
@@ -6,5 +7,9 @@ module.exports = class HttpResponse {
 
   static serverError () {
     return { statusCode: 500 }
+  }
+
+  static unauthorizedError () {
+    return { statusCode: 401, body: new UnauthorizedError() }
   }
 }
--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,7 @@
 
 const LoginRouter = require('../routers/login-router')
 const MissingParamsError = require('../helpers/missing-params-error')
+const UnauthorizedError = require('../helpers/unauthorized-error')
 
 const makeSut = () => {
   class AuthUseCaseSpy {
@@ -69,5 +70,6 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
+    expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
 })
