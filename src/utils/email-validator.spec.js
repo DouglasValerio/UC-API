5002e1662f1d1089a1ccb82837b26dee1f152090
Douglas Valério <douglas.valerio@matilha.digital>
2021-08-01 16:50:47 -0300

    Move System Under test into helper method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -4,15 +4,16 @@ class EmailValidator {
     return validator.isEmail(email)
   }
 }
+const makeSut = () => new EmailValidator()
 
 describe('Email Validator', () => {
   test('Should retur true if validator returns true', async () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_mail@mail.com')
     expect(isEmailValid).toBe(true)
   })
   test('Should retur false if validator returns false', async () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalid_mail@mail.com')
     expect(isEmailValid).toBe(false)
