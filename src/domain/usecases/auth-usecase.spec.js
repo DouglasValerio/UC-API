3b78fc443838486989b5d7bfdc5b094d52bb0c0b
Douglas Valério <douglas.valerio@matilha.digital>
2021-08-01 17:42:47 -0300

    Ensure that AuthUsecase calls FindUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,14 @@
 
 const { MissingParamsError } = require('../../utils/errors')
 class AuthUseCase {
+  constructor (findUserByEmailRepository) {
+    this.findUserByEmailRepository = findUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) throw new MissingParamsError('email')
     if (!password) throw new MissingParamsError('password')
+    await this.findUserByEmailRepository.findUser(email)
   }
 }
 
@@ -17,5 +22,16 @@ describe('Auth Usecase', () => {
     const sut = new AuthUseCase()
     const accessTokenPromise = sut.auth('any_mail@mail.com')
     expect(accessTokenPromise).rejects.toThrow(new MissingParamsError('password'))
+  })
+  test('Should call FindUserByEmailRepository with correct email', async () => {
+    class FindUserByEmailRepositorySpy {
+      async findUser (email) {
+        this.email = email
+      }
+    }
+    const findUserByEmailRepositorySpy = new FindUserByEmailRepositorySpy()
+    const sut = new AuthUseCase(findUserByEmailRepositorySpy)
+    await sut.auth('any_mail@mail.com', 'any_password')
+    expect(findUserByEmailRepositorySpy.email).toBe('any_mail@mail.com')
   })
 })
