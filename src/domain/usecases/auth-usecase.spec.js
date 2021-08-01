d12306f99a4c19d97ede73be51adbd17810c7c67
Douglas Valério <douglas.valerio@matilha.digital>
2021-08-01 20:41:11 -0300

    Ensure that AuthUsecase throws if an invalid repository is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,5 +1,5 @@
 
-const { MissingParamsError } = require('../../utils/errors')
+const { MissingParamsError, InvalidParamsError } = require('../../utils/errors')
 class AuthUseCase {
   constructor (findUserByEmailRepository) {
     this.findUserByEmailRepository = findUserByEmailRepository
@@ -8,6 +8,8 @@ class AuthUseCase {
   async auth (email, password) {
     if (!email) throw new MissingParamsError('email')
     if (!password) throw new MissingParamsError('password')
+    if (!this.findUserByEmailRepository) throw new MissingParamsError('findUserByEmailRepository')
+    if (!this.findUserByEmailRepository.findUser) throw new InvalidParamsError('findUserByEmailRepository')
     await this.findUserByEmailRepository.findUser(email)
   }
 }
@@ -37,5 +39,15 @@ describe('Auth Usecase', () => {
     const { sut, findUserByEmailRepositorySpy } = makeSut()
     await sut.auth('any_mail@mail.com', 'any_password')
     expect(findUserByEmailRepositorySpy.email).toBe('any_mail@mail.com')
+  })
+  test('Should throw if no repository is provided', async () => {
+    const sut = new AuthUseCase()
+    const authPromise = sut.auth('any_mail@mail.com', 'any_password')
+    expect(authPromise).rejects.toThrow(new MissingParamsError('findUserByEmailRepository'))
+  })
+  test('Should throw if findUserByEmailRepository has no findUser method', async () => {
+    const sut = new AuthUseCase({})
+    const authPromise = sut.auth('any_mail@mail.com', 'any_password')
+    expect(authPromise).rejects.toThrow(new InvalidParamsError('findUserByEmailRepository'))
   })
 })
