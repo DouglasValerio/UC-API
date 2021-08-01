1cd46c22ce40a5cf805be5ae105db3b312361539
Douglas Valério <douglas.valerio@matilha.digital>
2021-08-01 16:55:43 -0300

    Ensure that EmailValidator calls validator with correct parameter

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -7,15 +7,20 @@ class EmailValidator {
 const makeSut = () => new EmailValidator()
 
 describe('Email Validator', () => {
-  test('Should retur true if validator returns true', async () => {
+  test('Should return true if validator returns true', async () => {
     const sut = makeSut()
     const isEmailValid = sut.isValid('valid_mail@mail.com')
     expect(isEmailValid).toBe(true)
   })
-  test('Should retur false if validator returns false', async () => {
+  test('Should return false if validator returns false', async () => {
     const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalid_mail@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+  test('Should call validator with correct email', async () => {
+    const sut = makeSut()
+    sut.isValid('any_mail@mail.com')
+    expect(validator.email).toBe('any_mail@mail.com')
   })
 })
