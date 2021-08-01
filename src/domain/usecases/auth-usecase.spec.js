8593bee2f51a8a75a7a1cfab248405d72bc565b2
Douglas Valério <douglas.valerio@matilha.digital>
2021-08-01 20:30:51 -0300

    Move SUT creation to a helper factory

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -11,26 +11,30 @@ class AuthUseCase {
     await this.findUserByEmailRepository.findUser(email)
   }
 }
+const makeSut = () => {
+  class FindUserByEmailRepositorySpy {
+    async findUser (email) {
+      this.email = email
+    }
+  }
+  const findUserByEmailRepositorySpy = new FindUserByEmailRepositorySpy()
+  const sut = new AuthUseCase(findUserByEmailRepositorySpy)
+  return { sut, findUserByEmailRepositorySpy }
+}
 
 describe('Auth Usecase', () => {
   test('Should throw if no email is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const accessTokenPromise = sut.auth()
     expect(accessTokenPromise).rejects.toThrow(new MissingParamsError('email'))
   })
   test('Should throw if no password is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const accessTokenPromise = sut.auth('any_mail@mail.com')
     expect(accessTokenPromise).rejects.toThrow(new MissingParamsError('password'))
   })
   test('Should call FindUserByEmailRepository with correct email', async () => {
-    class FindUserByEmailRepositorySpy {
-      async findUser (email) {
-        this.email = email
-      }
-    }
-    const findUserByEmailRepositorySpy = new FindUserByEmailRepositorySpy()
-    const sut = new AuthUseCase(findUserByEmailRepositorySpy)
+    const { sut, findUserByEmailRepositorySpy } = makeSut()
     await sut.auth('any_mail@mail.com', 'any_password')
     expect(findUserByEmailRepositorySpy.email).toBe('any_mail@mail.com')
   })
